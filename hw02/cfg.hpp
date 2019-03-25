591664b5de4c0b9670d799d4fd8951cde1fbf184
Adam Vaňko <xvanko@fi.muni.cz>
2019-03-25 22:09:43 +0100

    hw02 CFG beginning

--- hw02/cfg.hpp
+++ hw02/cfg.hpp
@@ -1,12 +1,51 @@
 #include "../hw01/decode.hpp"
 
-class ControlFlowGraph {
-private:
+#include <unordered_set>
+#include <string>
+#include <vector>
 
+namespace
+{
+    using std::vector;
+    using InstructionDecoding::Instruction;
+    using std::string;
 
-public:
-    std::string generateDotSource(std::vector<InstructionDecoding::Instruction> instructions) {
-        return {};
-    }
+    class ControlFlowGraph {
+    private:
+        bool isControlFlowInstruction(const Instruction &instruction) {
+            const std::unordered_set<string> mnemonics = { "call", "jmp", "je", "jb", "jne" };
+            return mnemonics.find(instruction.mnemonic) != mnemonics.end();
+        }
 
-};
+    public:
+        string generateDotSource(const vector<Instruction>& instructions) {
+            using BasicBlock = vector<Instruction>;
+            std::unordered_map<int, BasicBlock> basicBlocks;
+            int labelIdCounter = 0;
+
+            int labelAddress = 0;
+            BasicBlock basicBlock;
+            for (int i = 0; i < instructions.size(); i++) {
+                const Instruction& instruction = instructions[i];
+                if (isControlFlowInstruction(instruction)) {
+                    basicBlocks[labelAddress] = basicBlock;
+
+                    int destAddress = i + instruction.operandA.value;
+                    auto it = basicBlocks.find(destAddress);
+                    if (it == basicBlocks.end()) {
+
+                    }
+                    else {
+
+                    }
+                } else {
+                    basicBlock.push_back(instruction);
+                }
+            }
+
+            return {};
+        }
+
+    };
+
+}
--- hw02/main.cpp
+++ hw02/main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 #include "cfg.hpp"
 #include "../hw01/decode.hpp"
 
 int main(int argc, char* argv[]) {
+    using InstructionDecoding::Instruction;
+    using std::string;
+    using std::cout;
+    using std::endl;
+    using std::cin;
+
+    InstructionDecoding::Decoder decoder;
+    std::vector<Instruction> instructions;
     if (argc > 1) {
-        std::string concatenatedBytes;
-        for (int i = 1; i < argc; i++) {
-            if (i != 1) {
-                concatenatedBytes += " ";
-            }
-            concatenatedBytes += std::string(argv[i]);
-        }
-        std::cout << InstructionDecoding::Decoder().decode(concatenatedBytes) << std::endl;
+        instructions = decoder.decodeInstructions(std::vector<const char*>(argv + 1, argv + argc));
     }
     else {
         std::vector<uint8_t> bytes;
         uint8_t byte;
-        while (std::cin >> byte) {
+        while (cin >> byte) {
             bytes.push_back(byte);
         }
+        instructions = decoder.decodeInstructions(bytes);
     }
+
+    ControlFlowGraph controlFlowGraph;
+    string dotSource = controlFlowGraph.generateDotSource(instructions);
+    cout << dotSource << endl;
 
     return 0;
 }
